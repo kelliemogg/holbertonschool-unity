09dda6d1d1854a2c2118e60ddf59a40617120f91
kelliemogg <kelliedmogg@gmail.com>
2021-09-07 14:31:27 -0800

    Player is moving using WASD and space bar to jump - no toppling

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float speed = 8.0f;
+    public GameObject player;
+    public float gravity = -8.0f;
+    public bool isGrounded;
+    public float jumpForce = 5.0f;
+
+    /// gets rb for GameObject
     void Start()
     {
-        if Key.Input == 'w' move left etc.
+        rb = GetComponent<Rigidbody>();
+        isGrounded = true;
+                //This locks the RigidBody so that it does not move or rotate in the Z axis.
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (isGrounded == true)
+        {
+            // GetAxis detects the user input (WASD or Arrows)
+            float xDirection = Input.GetAxis("Horizontal");
+            float zDirection = Input.GetAxis("Vertical");
+            float jump = Input.GetAxis("Jump");
+
+            // Vector3 is a variable that takes in 3 inputs (x, y, z)
+            Vector3 moveDirection = new Vector3(xDirection * speed, jump * jumpForce, zDirection * speed);
+
+            rb.AddForce(moveDirection * speed);
+        }
     }
 }
