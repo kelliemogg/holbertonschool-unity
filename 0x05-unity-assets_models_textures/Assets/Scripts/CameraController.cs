d254cf9579b0da6c7597f316a4b91bc27cdd3260
kelliemogg <kelliedmogg@gmail.com>
2021-09-08 14:34:44 -0800

    camera follows mouse - task 5

--- 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -7,42 +7,28 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Vector3 offset;
     public float smoothSpeed = 0.125f;
-
-    // camera reacts to click drag camera movement
-    public float dragSpeed = 2;
-    private Vector3 dragOrigin;
-
-    // camera reacts to mouse movement
-
-    private float yaw;
-    private float pitch;
+    public bool LookAtPlayer = false;
+    public bool RotateAroundPlayer = true;
+    public float RotationsSpeed = 5.0f;
 
     void Start()
     {
-        target = player.transform;
+        offset = transform.position - target.position;
     }
-    void FixedUpdate ()
+    void LateUpdate ()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-
-        if (Input.GetMouseButtonDown(0))
+        if (RotateAroundPlayer)
         {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * dragSpeed, -Input.GetAxis("Mouse X") * dragSpeed, 0));
-            return;
-        }
-        if (!Input.GetMouseButton(0))
-        {
-            return;
-        }
-        yaw = transform.rotation.eulerAngles.x;
-        pitch = transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Euler(yaw, pitch, 0);
+            Quaternion camTurnAngle =
+                Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationsSpeed, Vector3.up);
 
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
- 
-        transform.Rotate(-move, Space.World);
+            offset = camTurnAngle * offset;
+        }
+        
+        Vector3 newPos = target.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothSpeed);
+
+        if (LookAtPlayer || RotateAroundPlayer)
+            transform.LookAt(target);
     }
 }
