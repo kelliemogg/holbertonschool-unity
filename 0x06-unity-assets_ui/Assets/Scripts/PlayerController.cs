af8630517ba3c595e765cf12c40846498e6afc7a
kelliemogg <kelliedmogg@gmail.com>
2021-09-17 13:25:43 -0500

    starting point for proj 0x06

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform spawnPoint;
+    public GameObject cap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     
+        //cap.GetComponent<MeshRenderer>().material.color = Color.red;
+        if (player.transform.position.y < -6)
+        {
+            player.transform.position = spawnPoint.position;
+        }
+    }
+}
